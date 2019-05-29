4929136bf9a0426874518422700eb4206cfa9be1
Darshan Mistry <mistrydarshan1995@gmail.com>
2019-05-29 13:40:00 -0400

    Project homework-1

---
+++ coding/helloworld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout<<"Hi D@RSHAN MISTRY here....!"<<endl;
+return 0;
+}
---
+++ coding/print.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout<<"HELLO  D@RSHAN  MISTRY"<<endl;
+return 0;
+}
