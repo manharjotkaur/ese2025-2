497d0309eeba7efdf482eacf23e3cc92d5d0fdb0
Darshanmistry1995 <mistrydarshan1995@gmail.com>
2019-06-04 22:59:40 -0400

    Squre root

---
+++ coding/darshan.cpp
@@ -0,0 +1,19 @@
+//Find Squre root 
+//Darshan Mistry
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+        int x = 0;
+        while(x < 10) {
+                double y = sqrt((double)x);
+                cout << "The square root of " << x << " is " << y << endl;
+                x++;
+        }
+        return 0;
+}
+    
