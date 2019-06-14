1081605e3c1fe78d3abf89013d9282e6014c7143
Darshanmistry1995 <mistrydarshan1995@gmail.com>
2019-06-13 23:55:17 -0400

    Trunction Error

---
+++ coding/Trunction_error.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream> 
+    #include <cmath> 
+ 
+    using namespace std; 
+    int main() 
+   { 
+        double final,power,end_N = 1.0,e=1.0,x,n; 
+        
+        cout<< "x: ";          cin>> x;           cout<< "n: ";          cin>> n; 
+        
+        
+        while (n>1){             power = pow(x,n);             end_N = end_N*n;            e=(power/end_N)+e;  n--; 
+        }    
+final =e+x;       
+  cout<<"Truncation error Rn = "<< final<<endl; 
+      return 0;    } 
